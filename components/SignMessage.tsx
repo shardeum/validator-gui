f35a00be3c8603875576e5f88698ef9f2f297d54
CombatPug <p.ladar@icloud.com>
2023-01-19 17:14:10 +0100

    feat: add performance stats
    fix: public key sync after page load

--- components/SignMessage.tsx
+++ components/SignMessage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { ToastContext } from './ToastContextProvider';
@@ -77,6 +77,16 @@ export default function SignMessage({nominator, nominee}: { nominator: string, n
     nominee,
     timestamp: Date.now()
   });
+
+  useEffect(() => {
+      setData({
+        ...data,
+        nominator,
+        nominee
+      });
+    },
+    [nominator, nominee]
+  )
 
   // @ts-ignore
   window.ethereum.on("accountsChanged", (accounts: any) => {
@@ -158,7 +168,8 @@ export default function SignMessage({nominator, nominee}: { nominator: string, n
         {isLoading && <span className="h-5 w-5 inline ml-2">
             <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              <path className="opacity-75" fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
         </span>}
       </button>
