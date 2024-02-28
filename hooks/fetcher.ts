e14b984792d918c4c6a3f3a4a4d7e088d0137ae9
dnlbui <dnlbui@proton.me>
2024-02-28 11:02:09 -0600

    add extra info in error handling in fetcher.ts

--- hooks/fetcher.ts
+++ hooks/fetcher.ts
@@ -24,10 +24,8 @@ export const fetcher = <T>(input: RequestInfo | URL,
       if (isDev()) {
         console.error('Server Error (500) encountered for request:', input, 'with init:', init);
       }
-      // Generate a simple timestamp as an error identifier.
-      const errorId = new Date().toISOString();
 
-      showToast(`<span>Error ${res.status}: An unexpected error occurred while ${contextDescription}. Please report this issue to our support team and include this error code: ${errorId}. [<a href="https://github.com/Shardeum/shardeum-bug-reporting/issues" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Report Issue</a>]<span>`);
+      showToast(`<span>Error ${res.status}: An unexpected error occurred while ${contextDescription}. Detailed info for debugging: request: ${input}, with init: ${JSON.stringify(init)}. Please report this issue to our support team to investigate and resolve the problem. [<a href="https://github.com/Shardeum/shardeum-bug-reporting/issues" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Report Issue</a>]</span>`);
       return;
     } else if (!res.ok) {
       console.log(data.errorDetails);
