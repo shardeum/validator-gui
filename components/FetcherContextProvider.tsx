f53108c9d5d715e7360c7b6cbfe25fb578f79601
dnlbui <dnlbui@proton.me>
2024-03-07 13:39:03 -0600

    Revert changes to not use contextDescription

--- components/FetcherContextProvider.tsx
+++ components/FetcherContextProvider.tsx
@@ -1,30 +1,18 @@
 // FetcherContext.tsx
-import React, { createContext, ReactNode, useContext } from 'react'
-import { fetcher } from '../hooks/fetcher'
-import { ToastContext } from './ToastContextProvider'
+import React, { createContext, ReactNode, useContext } from 'react';
+import { fetcher } from '../hooks/fetcher';
+import { ToastContext } from './ToastContextProvider';
 
-type FetchFunction = <T>(input: RequestInfo | URL, init?: RequestInit, contextDescription?: string) => Promise<T>
+export const FetcherContext = createContext<typeof fetcher | null>(null);
 
-// Create a context that can hold a fetch function or null.
-export const FetcherContext = createContext<FetchFunction | null>(null)
+export default function FetcherContextProvider({children}: { children: ReactNode }) {
+  const {showErrorMessage} = useContext(ToastContext);
 
-// Component that provides a fetch function with additional context capabilities to its children.
-export default function FetcherContextProvider({ children }: { children: ReactNode }) {
-  const { showErrorMessage } = useContext(ToastContext)
-
-  // A wrapper function that adds context description to the fetcher function.
-  function fetcherWithContext<T>(
-    input: RequestInfo | URL,
-    init?: RequestInit,
-    contextDescription = 'fetching data'
-  ): Promise<T> {
-    return fetcher(input, init || {}, showErrorMessage, contextDescription)
+  function fetcherWithContext<T>(input: RequestInfo | URL, init: RequestInit): Promise<T> {
+    return fetcher(input, init, showErrorMessage);
   }
 
-  // Render the provider component, passing the fetcherWithContext function to its children.
   return (
-    <FetcherContext.Provider value={fetcherWithContext}>
-      {children}
-    </FetcherContext.Provider>
-  )
+    <FetcherContext.Provider value={fetcherWithContext}>{children}</FetcherContext.Provider>
+  );
 }
