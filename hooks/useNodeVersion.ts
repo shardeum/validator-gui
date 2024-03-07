1c3da5015cc4d7195ccec3aa4ace3d4c0e823570
dnlbui <dnlbui@proton.me>
2024-03-07 13:39:03 -0600

    Add error toast helper function and refactor error handling in fetcher.ts and useNodeVersion.ts

--- hooks/useNodeVersion.ts
+++ hooks/useNodeVersion.ts
@@ -14,27 +14,10 @@ type NodeVersionResult = {
 }
 
 export const useNodeVersion = (): NodeVersionResult => {
-  const { apiBase } = useGlobals();
-  const fetcherFromContext = useContext(FetcherContext)
-  const { showErrorMessage } = useContext(ToastContext)
-
-  if (!fetcherFromContext) {
-    throw new Error('FetcherContext not provided')
-  }
-
-  // Define a custom error handler for useSWR
-  const handleError = (error: Error, key: string) => {
-    const contextDescription = 'fetching node version';
-    showErrorMessage(`Error ${error.message}: An unexpected error occurred while ${contextDescription}. Please report this issue to our support team to investigate and resolve the problem. [<a href="https://github.com/Shardeum/shardeum-bug-reporting/issues" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Report Issue</a>]`)
-  }
-
-  const { data, error } = useSWR<NodeVersion, Error>(
-    `${apiBase}/api/node/version`,
-    (url) => fetcherFromContext(url, undefined, 'fetching node version') as Promise<NodeVersion>, // Explicitly cast the return type
-    {
-      onError: handleError,
-    }
-  )
+  const { apiBase } = useGlobals()
+  const fetcherWithContext = useContext(FetcherContext);
+  const { showErrorMessage } = useContext(ToastContext);
+  const { data, error } = useSWR<NodeVersion, Error>(`${apiBase}/api/node/version`, fetcherWithContext)
 
   const update = (): Promise<Response> => {
     return fetcher<Response>(`${apiBase}/api/node/update`, { method: 'POST' }, showErrorMessage)
