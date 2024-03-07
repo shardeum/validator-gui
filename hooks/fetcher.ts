a4a8c82b7f306deef69ae9d6954a9b434ea7e365
dnlbui <dnlbui@proton.me>
2024-03-07 13:39:03 -0600

    Refactor error message in fetcher.ts

--- hooks/fetcher.ts
+++ hooks/fetcher.ts
@@ -17,7 +17,12 @@ export const fetcher = <T>(input: RequestInfo | URL,
     if (res.status === 403) {
       authService.logout(apiBase);
     } else if (res.status === 500) {
-      showToast('<span>Sorry, something went wrong. Please report this issue to our support team so we can investigate and resolve the problem.</span>');
+      // Environment check to ensure detailed logging is done only in development
+      if (isDev()) {
+        console.error('Server Error (500) encountered for request:', input, 'with init:', init);
+      }
+      
+      showToast(`<span>Error ${res.status}: An unexpected error occurred while ${contextDescription}. Please report this issue to our support team to investigate and resolve the problem. [<a href="https://github.com/Shardeum/shardeum-bug-reporting/issues" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Report Issue</a>]<span>`);
       return;
     } else if (!res.ok) {
       console.log(data.errorDetails);
