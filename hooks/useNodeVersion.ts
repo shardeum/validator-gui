304d9a38a71f6840b98d8806d38499f2a652121e
dnlbui <dnlbui@proton.me>
2024-03-07 13:39:03 -0600

    Refactor fetcherWithContext function and add error handling in useNodeVersion hook

--- hooks/useNodeVersion.ts
+++ hooks/useNodeVersion.ts
@@ -2,9 +2,9 @@ import useSWR from 'swr'
 import { fetcher } from './fetcher'
 import { NodeVersion } from '../model/node-version'
 import { useGlobals } from '../utils/globals'
-import { useContext } from 'react';
-import { FetcherContext } from '../components/FetcherContextProvider';
-import { ToastContext } from '../components/ToastContextProvider';
+import { useContext } from 'react'
+import { FetcherContext } from '../components/FetcherContextProvider'
+import { ToastContext } from '../components/ToastContextProvider'
 
 type NodeVersionResult = {
   isLoading: boolean
@@ -14,10 +14,27 @@ type NodeVersionResult = {
 }
 
 export const useNodeVersion = (): NodeVersionResult => {
-  const { apiBase } = useGlobals()
-  const fetcherWithContext = useContext(FetcherContext);
-  const { showErrorMessage } = useContext(ToastContext);
-  const { data, error } = useSWR<NodeVersion, Error>(`${apiBase}/api/node/version`, fetcherWithContext)
+  const { apiBase } = useGlobals();
+  const fetcherFromContext = useContext(FetcherContext)
+  const { showErrorMessage } = useContext(ToastContext)
+
+  if (!fetcherFromContext) {
+    throw new Error('FetcherContext not provided')
+  }
+
+  // Define a custom error handler for useSWR
+  const handleError = (error: Error, key: string) => {
+    const contextDescription = 'fetching node version';
+    showErrorMessage(`Error ${error.message}: An unexpected error occurred while ${contextDescription}. Please report this issue to our support team to investigate and resolve the problem. [<a href="https://github.com/Shardeum/shardeum-bug-reporting/issues" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">Report Issue</a>]`)
+  }
+
+  const { data, error } = useSWR<NodeVersion, Error>(
+    `${apiBase}/api/node/version`,
+    (url) => fetcherFromContext(url, undefined, 'fetching node version') as Promise<NodeVersion>, // Explicitly cast the return type
+    {
+      onError: handleError,
+    }
+  )
 
   const update = (): Promise<Response> => {
     return fetcher<Response>(`${apiBase}/api/node/update`, { method: 'POST' }, showErrorMessage)
