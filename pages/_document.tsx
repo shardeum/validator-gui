f12d2c105269be2b05f36e23838738a2ac1763ad
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-05-09 15:40:11 +0530

    changed login page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="forest">
-      <Head/>
-      <body className="bg-transparent">
-      <Main/>
-      <NextScript/>
+    <Html className="h-full bg-gray-50">
+      <Head />
+      <body className="bg-transparent h-full">
+        <Main />
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,78 +1,73 @@
-import { ReactElement, SetStateAction, useState } from 'react'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { FieldValues, useForm } from 'react-hook-form'
-import { ArrowPathIcon } from '@heroicons/react/20/solid'
-import { authService } from '../../services/auth.service';
-import Head from 'next/head';
+import { ReactElement } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { GeistSans } from "geist/font";
+import { authService } from "../../services/auth.service";
+import Head from "next/head";
+import Image from "next/image";
+import logoIconOtherBlocks from "../../assets/login-aside-other-blocks.svg";
+import { Logo } from "../../components/atoms/Logo";
+import { Title } from "../../components/atoms/Title";
+import { LoginForm } from "../../components/molecules/LoginForm";
 
 const Login = () => {
-  const router = useRouter()
-  const login = authService.useLogin()
+  const router = useRouter();
 
   useEffect(() => {
     // redirect to home if already logged in
     if (authService.isLogged) {
-      router.push('/')
+      router.push("/");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  const {register, handleSubmit, formState} = useForm()
-
-  const [apiError, setApiError] = useState<Error | null>(null);
-
-  async function onSubmit({password}: FieldValues) {
-    setApiError(null);
-
-    try{
-      await login(password)
-      router.push('/')
-    }
-    catch(error){
-      setApiError(error as SetStateAction<Error | null>)
-    }
-  }
+  }, []);
 
   return (
-    <>
+    <div className="h-screen w-screen flex center">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="logo.png" alt="Logo" className="w-40 mb-5 mt-20"/>
-      <div className="bg-white text-stone-500	rounded-xl p-8 text-sm [&>*]:pb-2 max-w-xl">
-        <h1 className="text-black font-semibold text-2xl">Connect to Validator Dashboard</h1>
-        <p>
-          Connect to your validator dashboard to see the performance of your node, check rewards and run
-          maintenance tasks!
-        </p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register('password')} placeholder="Password" type="password"
-                 className="block p-4 w-full bg-stone-200 text-stone-600 my-2"></input>
-          {apiError && (
-            <div className="flex text-red-500 items-center mb-5">
-              <div className="ml-2 font-semibold">{apiError.message}</div>
-            </div>
-          )}
-          <button disabled={formState.isSubmitting} className="btn btn-primary" type="submit">
-            {formState.isSubmitting ? <ArrowPathIcon className='w-5 spinner'/> : 'Connect'}
-          </button>
-        </form>
+      <aside className="w-full h-full grow basis-0 bg-[#3042FB] text-white">
+        <Image
+          src={logoIconOtherBlocks}
+          alt="My SVG"
+          className="h-full w-full"
+        />
+      </aside>
+      <div className="bg-white grow text-stone-500 basis-0	rounded-xl text-sm">
+        <main className="grow p-20">
+          <div className="flex flex-col gap-y-10">
+            <Logo className="w-36" />
+            <Title
+              text="Enter password to connect to the validator dashboard"
+              className={
+                GeistSans.className +
+                " text-3xl font-semibold text-gray-900 max-w-[400px]"
+              }
+            />
+            <LoginForm />
+          </div>
+        </main>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 Login.getLayout = function getLayout(page: ReactElement) {
-  return <>
-    <Head>
-      <title>Shardeum Dashboard</title>
-      <meta name="description" content="Dashboard to configure a Shardeum validator"/>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-      <link rel="icon" href="/favicon.ico"/>
-    </Head>
-    <div className="grid justify-center">
-      {page}
-    </div>
-  </>
-}
+  return (
+    <>
+      <Head>
+        <title>Shardeum Dashboard</title>
+        <meta
+          name="description"
+          content="Dashboard to configure a Shardeum validator"
+        />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>{page}</div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
