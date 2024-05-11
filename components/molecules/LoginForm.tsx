040d4071198e6ceea4c7fd603240ae63c5ab9d62
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-05-11 11:20:17 +0530

    added copy text for login page

--- components/molecules/LoginForm.tsx
+++ components/molecules/LoginForm.tsx
@@ -6,13 +6,11 @@ import { GeistSans } from "geist/font";
 import { authService } from "../../services/auth.service";
 import { useRouter } from "next/router";
 import { ArrowPathIcon } from "@heroicons/react/20/solid";
-import { useNodeVersion } from "../../hooks/useNodeVersion";
 
 export const LoginForm: React.FC = () => {
   const { register, handleSubmit, formState } = useForm();
   const [isInputVisible, setIsInputVisible] = useState(false);
   const [isActive, setIsActive] = useState(false);
-  const { publicVersion } = useNodeVersion();
 
   const handleToggle = () => {
     if (isInputVisible) {
@@ -39,83 +37,75 @@ export const LoginForm: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col gap-y-48">
-      <div className="flex flex-col shadow-md border border-gray-100 rounded-md px-2 py-2">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
-          <label className={GeistSans.className + " block"}>
-            <span className="text-gray-900 font-light bg-white">Password</span>
-          </label>
-          <div>
-            <div
-              className={
-                "flex items-center py-2 px-3 rounded-md border border-b-2 bg-white " +
-                (isActive ? "border-b-indigo-500" : "")
-              }
-            >
-              <div className="w-full flex justify-between">
-                <input
-                  type={isInputVisible ? "text" : "password"}
-                  placeholder="Enter password here"
-                  className="outline-none flex-1 bg-white"
-                  {...register("password")}
-                  onChange={(e) => {
-                    const password = e.target.value;
-                    if (password.length > 0) {
-                      setIsActive(true);
-                    } else {
-                      setIsActive(false);
-                    }
-                  }}
-                />
-                <span>
-                  {isInputVisible ? (
-                    <EyeIcon
-                      className="cursor-pointer h-4 ml-2"
-                      onClick={handleToggle}
-                    />
-                  ) : (
-                    <EyeSlashIcon
-                      className="cursor-pointer h-4 ml-2"
-                      onClick={handleToggle}
-                    />
-                  )}
-                </span>
+    <div className="flex flex-col shadow-md border border-gray-100 rounded-md px-2 py-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+        <label className={GeistSans.className + " block"}>
+          <span className="text-gray-900 font-light bg-white">Password</span>
+        </label>
+        <div>
+          <div
+            className={
+              "flex items-center py-2 px-3 rounded-md border border-b-2 bg-white " +
+              (isActive ? "border-b-indigo-500" : "")
+            }
+          >
+            <div className="w-full flex justify-between">
+              <input
+                type={isInputVisible ? "text" : "password"}
+                placeholder="Enter password here"
+                className="outline-none flex-1 bg-white"
+                {...register("password")}
+                onChange={(e) => {
+                  const password = e.target.value;
+                  if (password.length > 0) {
+                    setIsActive(true);
+                  } else {
+                    setIsActive(false);
+                  }
+                }}
+              />
+              <span>
+                {isInputVisible ? (
+                  <EyeIcon
+                    className="cursor-pointer h-4 ml-2"
+                    onClick={handleToggle}
+                  />
+                ) : (
+                  <EyeSlashIcon
+                    className="cursor-pointer h-4 ml-2"
+                    onClick={handleToggle}
+                  />
+                )}
+              </span>
+            </div>
+          </div>
+          {apiError && (
+            <div className="flex text-red-600 items-center mb-5 mt-1">
+              <div className={"font-normal text-xs " + GeistSans.className}>
+                {apiError.message}
               </div>
             </div>
-            {apiError && (
-              <div className="flex text-red-600 items-center mb-5 mt-1">
-                <div className={"font-normal text-xs " + GeistSans.className}>
-                  {apiError.message}
-                </div>
-              </div>
-            )}
-          </div>
+          )}
+        </div>
 
-          <div className="flex justify-end">
-            <button
-              disabled={formState.isSubmitting}
-              className={
-                (isActive
-                  ? "bg-indigo-600 hover:bg-indigo-700"
-                  : "bg-gray-300") +
-                " text-white text-sm font-semibold py-2 px-4 rounded-md " +
-                GeistSans.className
-              }
-              type="submit"
-            >
-              {formState.isSubmitting ? (
-                <ArrowPathIcon className="w-5 spinner" />
-              ) : (
-                "Connect Securely"
-              )}
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="flex justify-between">
-        <span>Validator Version : {publicVersion?.runningCliVersion}</span>
-        <span>Running on Localhost</span>
-      </div>
+        <div className="flex justify-end">
+          <button
+            disabled={formState.isSubmitting}
+            className={
+              (isActive ? "bg-indigo-600 hover:bg-indigo-700" : "bg-gray-300") +
+              " text-white text-sm font-semibold py-2 px-4 rounded-md " +
+              GeistSans.className
+            }
+            type="submit"
+          >
+            {formState.isSubmitting ? (
+              <ArrowPathIcon className="w-5 spinner" />
+            ) : (
+              "Connect Securely"
+            )}
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
