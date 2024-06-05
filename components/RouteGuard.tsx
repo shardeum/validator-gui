e5ea31466ecb6ce426eed43661c348b301f05ad4
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-06-05 13:26:38 +0530

    added assets and formatting changes

--- components/RouteGuard.tsx
+++ components/RouteGuard.tsx
@@ -1,45 +1,45 @@
-import { useState, useEffect, ReactNode } from 'react';
-import { useRouter } from 'next/router';
-import { authService } from '../services/auth.service';
+import { useState, useEffect, ReactNode } from "react";
+import { useRouter } from "next/router";
+import { authService } from "../services/auth.service";
 
 export default function RouteGuard({ children }: { children: ReactNode }) {
-    const router = useRouter();
-    const [authorized, setAuthorized] = useState(false);
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
 
-    useEffect(() => {
-        // on initial load - run auth check
-        authCheck(router.asPath);
+  useEffect(() => {
+    // on initial load - run auth check
+    authCheck(router.asPath);
 
-        // on route change start - hide page content by setting authorized to false
-        const hideContent = () => setAuthorized(false);
-        router.events.on('routeChangeStart', hideContent);
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false);
+    router.events.on("routeChangeStart", hideContent);
 
-        // on route change complete - run auth check
-        router.events.on('routeChangeComplete', authCheck)
+    // on route change complete - run auth check
+    router.events.on("routeChangeComplete", authCheck);
 
-        // unsubscribe from events in useEffect return function
-        return () => {
-            router.events.off('routeChangeStart', hideContent);
-            router.events.off('routeChangeComplete', authCheck);
-        }
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off("routeChangeStart", hideContent);
+      router.events.off("routeChangeComplete", authCheck);
+    };
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    function authCheck(url: string) {
-        // redirect to login page if accessing a private page and not logged in
-        const publicPaths = ['/login'];
-        const path = url.split('?')[0];
-        if (!authService.isLogged && !publicPaths.includes(path)) {
-            setAuthorized(false);
-            router.push({
-                pathname: '/login',
-                query: { returnUrl: router.asPath }
-            });
-        } else {
-            setAuthorized(true);
-        }
+  function authCheck(url: string) {
+    // redirect to login page if accessing a private page and not logged in
+    const publicPaths = ["/login"];
+    const path = url.split("?")[0];
+    if (!authService.isLogged && !publicPaths.includes(path)) {
+      setAuthorized(false);
+      router.push({
+        pathname: "/login",
+        query: { returnUrl: router.asPath },
+      });
+    } else {
+      setAuthorized(true);
     }
+  }
 
   return <>{authorized && children}</>;
 }
