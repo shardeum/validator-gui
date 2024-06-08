eaf5c220ecb3e3ab2694e22a5903fd3002c192d1
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-06-08 10:14:04 +0530

    mobile responsiveness changes

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { BellIcon } from "@heroicons/react/24/solid";
+import { ArrowRightOnRectangleIcon, BellIcon } from "@heroicons/react/24/solid";
 import { Logo } from "../../components/atoms/Logo";
 import { Cog6ToothIcon } from "@heroicons/react/20/solid";
 import { WalletConnectButton } from "../../components/molecules/WalletConnectButton";
@@ -18,8 +18,14 @@ import { NotificationsWindow } from "../../components/molecules/NotificationsWin
 import useNotificationsStore from "../../hooks/useNotificationsStore";
 import { ToastWindow } from "../../components/molecules/ToastWindow";
 import { useDevice } from "../../context/device";
-import { NodeStatusMobileRibbon } from "../../components/molecules/NodeStatusMobileRibbon";
+import { NodeStatusRibbon } from "../../components/molecules/NodeStatusRibbon";
 import { useAccount } from "wagmi";
+import { MobileModalWrapper } from "../../components/layouts/MobileModalWrapper";
+import useModalStore from "../../hooks/useModalStore";
+import { MobileMenu } from "../../components/molecules/MobileMenu";
+import { authService } from "../../services/auth.service";
+import { useGlobals } from "../../utils/globals";
+import { InformationPopupsDisplay } from "../../components/molecules/InformationPopupsDisplay";
 
 enum Content {
   MAIN = "MAIN",
@@ -38,22 +44,19 @@ const Dashboard = () => {
   }));
 
   const [contentPane, setContentPane] = useState<Content>(Content.MAIN);
-  const toggleSettingsDisplay = () => {
-    if (contentPane !== Content.SETTINGS) {
-      setContentPane(Content.SETTINGS);
-    } else {
-      setContentPane(Content.MAIN);
-    }
+  const setToSettingsDisplay = () => {
+    setContentPane(Content.SETTINGS);
   };
-  const toggleLogsDisplay = () => {
-    if (contentPane !== Content.LOGS) {
-      setContentPane(Content.LOGS);
-    } else {
-      setContentPane(Content.MAIN);
-    }
+  const setToLogsDisplay = () => {
+    setContentPane(Content.LOGS);
   };
   const { isMobile } = useDevice();
   const { isConnected } = useAccount();
+  const { setShowModal, setContent } = useModalStore((state: any) => ({
+    setShowModal: state.setShowModal,
+    setContent: state.setContent,
+  }));
+  const { apiBase } = useGlobals();
 
   return (
     <>
@@ -74,23 +77,32 @@ const Dashboard = () => {
                     />
                     <NotificationsWindow />
                   </div>
-                  {/* TODO: find an appropriate icon */}
                   <button
                     className="fill-bg h-4 w-4"
-                    onClick={toggleLogsDisplay}
+                    onClick={setToLogsDisplay}
                     style={{
                       backgroundImage: `url(${notebookIcon.src})`,
                     }}
                   ></button>
                   <Cog6ToothIcon
-                    onClick={toggleSettingsDisplay}
+                    onClick={setToSettingsDisplay}
                     className="w-5 h-5 text-black bg-white cursor-pointer"
                   />
                   <WalletConnectButton
                     toShowAddress={true}
                     label="Connect Wallet"
                   ></WalletConnectButton>
-                  <ToastWindow />
+                  <ArrowRightOnRectangleIcon
+                    className="h-5 w-5 text-black cursor-pointer tooltip tooltip-bottom"
+                    data-tip="Logout"
+                    onClick={async () => await authService.logout(apiBase)}
+                  />
+                  <ToastWindow
+                    viewLogsOnClick={setToLogsDisplay}
+                    supportOnClick={() => {
+                      //pass
+                    }}
+                  />
                 </div>
               </div>
               <div className="flex justify-start gap-x-3 text-black text-xs mb-2">
@@ -129,21 +141,15 @@ const Dashboard = () => {
           </nav>
 
           {/* content */}
-          <div
-            className={
-              "w-full flex flex-col text-black relative scroll-smooth " +
-              (contentPane === Content.SETTINGS ? "bg-subtleBg" : "")
-            }
-          >
+          <div className="w-full flex flex-col text-black relative scroll-smooth">
             <div className="flex pr-[32rem]">
               {/* dashboard metrics */}
               <div className="grow h-screen pt-32 w-full flex flex-col justify-between">
                 {contentPane === Content.MAIN && (
                   <>
                     <div className="px-16 flex flex-col gap-y-12">
-                      <span className="font-medium text-3xl">
-                        Welcome Back!
-                      </span>
+                      <InformationPopupsDisplay />
+
                       {/* Rewards */}
                       <section
                         id="overview"
@@ -206,11 +212,11 @@ const Dashboard = () => {
       {isMobile && (
         <div className="flex flex-col text-black">
           {/* navbar */}
-          <nav className="px-4 shadow fixed top-0 bg-white w-full z-10 h-14">
+          <nav className="px-4 shadow fixed top-0 bg-white w-full z-10 h-20">
             <div className="flex flex-col w-full">
               <div className="flex justify-between py-3 w-full">
                 <Logo className="w-8" isMinimalLogo={true} />
-                <div className="flex items-center gap-x-4 relative">
+                <div className="flex items-center gap-x-3 relative">
                   <WalletConnectButton
                     toShowAddress={true}
                     label="Connect Wallet"
@@ -218,66 +224,124 @@ const Dashboard = () => {
                   <BellIcon
                     className="w-5 h-5 text-black cursor-pointer"
                     onClick={() => {
-                      setShowWindow(!showWindow);
+                      setShowWindow(true);
+                      setContent(
+                        <MobileModalWrapper
+                          closeButtonRequired={false}
+                          contentOnTop={false}
+                          wrapperClassName="fixed bottom-0 flex flex-col pt-7 items-center justify-start rounded-t-2xl min-h-1/2 overflow-scroll bg-white w-screen dropdown-300 text-black"
+                        >
+                          <NotificationsWindow />
+                        </MobileModalWrapper>
+                      );
+                      setShowModal(true);
                     }}
                   />
-                  <Bars3Icon className="h-5 w-5 text-black cursor-pointer" />
+                  <ArrowRightOnRectangleIcon
+                    className="h-5 w-5 text-black cursor-pointer tooltip tooltip-bottom"
+                    data-tip="Logout"
+                    onClick={async () => await authService.logout(apiBase)}
+                  />
+                  <Bars3Icon
+                    className="h-5 w-5 text-black cursor-pointer"
+                    onClick={() => {
+                      setContent(
+                        <MobileModalWrapper
+                          closeButtonRequired={true}
+                          contentOnTop={true}
+                        >
+                          <MobileMenu
+                            logsOnClick={() => {
+                              setToLogsDisplay();
+                              setShowModal(false);
+                            }}
+                            settingsOnClick={() => {
+                              setToSettingsDisplay();
+                              setShowModal(false);
+                            }}
+                          />
+                        </MobileModalWrapper>
+                      );
+                      setShowModal(true);
+                    }}
+                  />
                 </div>
               </div>
+              {contentPane !== Content.MAIN && (
+                <button
+                  className="flex gap-x-1 items-center rounded font-semibold text-sm ease-in-out duration-200"
+                  onClick={() => {
+                    setContentPane(Content.MAIN);
+                  }}
+                >
+                  <ChevronLeftIcon className="h-3 w-3 text-black stroke-2" />
+                  <span className="text-sm">Dashboard</span>
+                </button>
+              )}
             </div>
+            <ToastWindow
+              viewLogsOnClick={setToLogsDisplay}
+              supportOnClick={() => {
+                //pass
+              }}
+            />
           </nav>
-          <NodeStatusMobileRibbon isWalletConnected={isConnected} />
+          <NodeStatusRibbon isWalletConnected={isConnected} />
           {/* dashboard metrics */}
           <div className="grow h-screen pt-8 w-full flex flex-col justify-between">
-            <>
-              <div className="px-4 flex flex-col gap-y-12">
-                <span className="font-medium text-3xl">Welcome Back!</span>
-                {/* Rewards */}
-                <section
-                  id="overview"
-                  ref={overviewSectionRef}
-                  className="flex flex-col w-full"
-                >
-                  <span className="font-medium text-base mb-1">
-                    Your Rewards
-                  </span>
-                  <RewardsCard />
-                </section>
-
-                {/* Network Size */}
-                <section
-                  id="network-size"
-                  ref={networkSizeSectionRef}
-                  className="flex flex-col w-full"
-                >
-                  <span className="font-medium text-base mb-1">
-                    Network Size
-                  </span>
-                  <NetworkSizeCard />
-                </section>
-
-                {/* Performance */}
-                <section id="performance" ref={performanceSectionRef}>
-                  <div className="flex items-center gap-x-3">
+            {contentPane === Content.MAIN && (
+              <>
+                <div className="px-4 flex flex-col gap-y-12">
+                  <InformationPopupsDisplay />
+                  {/* Rewards */}
+                  <section
+                    id="overview"
+                    ref={overviewSectionRef}
+                    className="flex flex-col w-full"
+                  >
                     <span className="font-medium text-base mb-1">
-                      Performance
+                      Your Rewards
                     </span>
-                    <span className="text-xs text-gray-500">
-                      Average Usage*
+                    <RewardsCard />
+                  </section>
+
+                  {/* Network Size */}
+                  <section
+                    id="network-size"
+                    ref={networkSizeSectionRef}
+                    className="flex flex-col w-full"
+                  >
+                    <span className="font-medium text-base mb-1">
+                      Network Size
                     </span>
-                  </div>
-                  <PerformanceDisplay />
-                </section>
-              </div>
-              {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
-              <img
-                src={dashboardBg.src}
-                className="w-full"
-                style={{
-                  backgroundImage: `url(${dashboardBg.src})`,
-                }}
-              ></img>
-            </>
+                    <NetworkSizeCard />
+                  </section>
+
+                  {/* Performance */}
+                  <section id="performance" ref={performanceSectionRef}>
+                    <div className="flex items-center gap-x-3">
+                      <span className="font-medium text-base mb-1">
+                        Performance
+                      </span>
+                      <span className="text-xs text-gray-500">
+                        Average Usage*
+                      </span>
+                    </div>
+                    <PerformanceDisplay />
+                  </section>
+                </div>
+                {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
+                <img
+                  src={dashboardBg.src}
+                  className="w-full"
+                  style={{
+                    backgroundImage: `url(${dashboardBg.src})`,
+                  }}
+                ></img>
+              </>
+            )}
+            {contentPane === Content.SETTINGS && <SettingsDisplay />}
+            {contentPane === Content.LOGS && <LogsDisplay />}
           </div>
         </div>
       )}
