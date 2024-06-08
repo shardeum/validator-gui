9cc7e8fce9ed5937504634aaa22c7d42cf0b6f00
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-06-08 10:14:04 +0530

    mobile responsiveness changes

--- components/molecules/ToastWindow.tsx
+++ components/molecules/ToastWindow.tsx
@@ -1,20 +1,30 @@
 import useToastStore from "../../hooks/useToastStore";
 import { ToastBox } from "../atoms/ToastBox";
 
-export const ToastWindow = () => {
+type ToastWindowProps = {
+  viewLogsOnClick: () => void;
+  supportOnClick: () => void;
+};
+
+export const ToastWindow = ({
+  viewLogsOnClick,
+  supportOnClick,
+}: ToastWindowProps) => {
   const { currentToast, resetToast } = useToastStore((state: any) => ({
     currentToast: state.currentToast,
     resetToast: state.resetToast,
   }));
 
   return (
-    <div className="absolute top-8 right-0 z-35">
+    <div className="absolute top-19 max-md:w-full md:top-8 right-0 z-35">
       {currentToast && (
         <ToastBox
           toast={currentToast}
           onClose={() => {
             resetToast();
           }}
+          viewLogsOnClick={viewLogsOnClick}
+          supportOnClick={supportOnClick}
         />
       )}
     </div>
