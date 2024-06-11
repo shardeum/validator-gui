c8c70cb60204aa7bd7bc5bcab0dab3268c142f68
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-06-11 20:26:43 +0530

    passing required usePassword hook params

--- components/ResetPasswordForm.tsx
+++ components/ResetPasswordForm.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import LoadingButton from './LoadingButton';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { usePassword } from '../hooks/usePasswordChange';
+import React from "react";
+import { useForm } from "react-hook-form";
+import LoadingButton from "./LoadingButton";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { usePassword } from "../hooks/usePasswordChange";
 
 type FormData = {
   currentPassword: string;
@@ -11,10 +11,16 @@ type FormData = {
 };
 
 const ResetPasswordForm = () => {
-  const {register, handleSubmit, watch, formState: {errors, isValid}} = useForm<FormData>({
-    mode: 'onChange',
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setError,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    mode: "onChange",
   });
-  const {changePassword, isLoading} = usePassword();
+  const { changePassword, isLoading } = usePassword({ setError });
 
   const onSubmit = (data: FormData) => {
     changePassword(data.currentPassword, data.newPassword);
@@ -33,12 +39,14 @@ const ResetPasswordForm = () => {
             className="bg-white text-black border px-1"
             id="currentPassword"
             type="password"
-            {...register("currentPassword", {required: "This field is required"})}
+            {...register("currentPassword", {
+              required: "This field is required",
+            })}
           />
         </div>
         <span className="text-right text-sm text-warning">
-        {errors.currentPassword && <p>{errors.currentPassword.message}</p>}
-      </span>
+          {errors.currentPassword && <p>{errors.currentPassword.message}</p>}
+        </span>
 
         <div className="flex justify-between mt-2">
           <label htmlFor="newPassword">New Password</label>
@@ -46,12 +54,12 @@ const ResetPasswordForm = () => {
             className="bg-white text-black border px-1"
             id="newPassword"
             type="password"
-            {...register("newPassword", {required: "This field is required"})}
+            {...register("newPassword", { required: "This field is required" })}
           />
         </div>
         <span className="text-right text-sm text-warning">
-        {errors.newPassword && <p>{errors.newPassword.message}</p>}
-      </span>
+          {errors.newPassword && <p>{errors.newPassword.message}</p>}
+        </span>
 
         <div className="flex justify-between mt-2">
           <label htmlFor="confirmNewPassword">Confirm Password</label>
@@ -61,26 +69,31 @@ const ResetPasswordForm = () => {
             type="password"
             {...register("confirmNewPassword", {
               required: "This field is required",
-              validate: value =>
-                value === password || "The passwords do not match"
+              validate: (value) =>
+                value === password || "The passwords do not match",
             })}
           />
         </div>
         <span className="text-right text-sm text-warning">
-        {errors.confirmNewPassword && <p>{errors.confirmNewPassword.message}</p>}
-      </span>
+          {errors.confirmNewPassword && (
+            <p>{errors.confirmNewPassword.message}</p>
+          )}
+        </span>
 
         <div className="flex justify-end mt-2">
-          <LoadingButton className="btn btn-primary"
-                         isLoading={isLoading}
-                         disabled={!isValid}
-                         type="submit">
+          <LoadingButton
+            className="btn btn-primary"
+            isLoading={isLoading}
+            disabled={!isValid}
+            type="submit"
+          >
             Submit
-            <ArrowRightIcon className="h-5 w-5 inline ml-2"/>
+            <ArrowRightIcon className="h-5 w-5 inline ml-2" />
           </LoadingButton>
         </div>
       </form>
-    </>);
-}
+    </>
+  );
+};
 
 export default ResetPasswordForm;
