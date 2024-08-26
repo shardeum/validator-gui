86bfb6345a87b6a0c25935cd2d6ef01bc57ff6da
soniasingla <soniasingla.1812@gmail.com>
2024-08-26 11:09:41 +0200

    Generates logs, fixes download and clear logs button, popup issue and closes navbar by clicking anywhere on the screen
    add alert for successful password reset

--- components/molecules/NotificationsWindow.tsx
+++ components/molecules/NotificationsWindow.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from "react";
 import useNotificationsStore, {
   NotificationInstance,
 } from "../../hooks/useNotificationsStore";
@@ -12,10 +13,32 @@ export const NotificationsWindow = () => {
       resetNotifications: state.resetNotifications,
     }));
 
+  const windowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Function to close the window if clicked outside of it
+    const handleClickOutside = (event: MouseEvent) => {
+      if (windowRef.current && !windowRef.current.contains(event.target as Node)) {
+        setShowWindow(false);
+      }
+    };
+
+    // Add event listener for clicks
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setShowWindow]);
+
   return (
     <div className="md:absolute md:top-5 md:right-0 z-40">
       {showWindow && (
-        <div className="flex flex-col max-md:grow bg-white md:border md:p-3 text-black md:rounded md:shadow min-w-[20rem] max-h-[85vh] md:max-h-[60vh] overflow-scroll max-md:px-2">
+        <div
+          ref={windowRef}
+          className="flex flex-col max-md:grow bg-white md:border md:p-3 text-black md:rounded md:shadow min-w-[20rem] max-h-[85vh] md:max-h-[60vh] overflow-scroll max-md:px-2"
+        >
           {/* header */}
           <div className="flex justify-between items-center px-1 md:mb-3 mb-7">
             <div className="flex gap-x-2 items-center">
