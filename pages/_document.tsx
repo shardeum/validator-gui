9a1a9bb8feb09802af7f52fef7068348392b77be
CombatPug <pe.ladar@icloud.com>
2024-09-06 19:44:00 +0200

    fix double html body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50">
-      <Head />
+      <Head >
+        <meta
+          name="description"
+          content="Dashboard to configure a Shardeum validator"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="bg-transparent h-full">
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
       </body>
     </Html>
   );
