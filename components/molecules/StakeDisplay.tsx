02d6259d1d8bc56e09b2a52939cf02e5b09ec454
Sam Sweet <sam.w.sweet@gmail.com>
2024-10-08 09:20:01 -0400

    Min Requirement should not show how much is left to be stakes but rather the static total needed

--- components/molecules/StakeDisplay.tsx
+++ components/molecules/StakeDisplay.tsx
@@ -70,7 +70,9 @@ export const StakeDisplay = () => {
           </span>
           <div className="flex gap-x-1">
             <span className="font-light text-xs">Min. requirement: </span>
-            <span className="text-xs">{minimumStakeRequirement} SHM</span>
+            <span className="text-xs">
+              {nodeStatus?.stakeRequirement || "10"} SHM
+            </span>
           </div>
         </div>
         <hr className="my-1 mx-3" />
